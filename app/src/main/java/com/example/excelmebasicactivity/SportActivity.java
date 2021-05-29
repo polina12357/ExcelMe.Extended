de7b51d3e2d50e173d05c9924b1ad0d2392e7abf
polina12357 <fedorenko.polinka@gmail.com>
2021-05-29 16:48:36 +0300

    design changes

---
+++ app/src/main/java/com/example/excelmebasicactivity/SportActivity.java
@@ -0,0 +1,106 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		excelme_design
+	 *	@date 		1622291072901
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.excelmebasicactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SportActivity extends Activity {
+
+	
+	private View _bg__sport;
+	private ImageView background_1;
+	private View rectangle_29;
+	private View rectangle_30;
+	private ImageView logotextg_1;
+	private ImageView arrow_1;
+	private TextView legs;
+	private ImageView legs_1;
+	private ImageView leg_1;
+	private TextView arms;
+	private ImageView arm_1;
+	private TextView abs;
+	private ImageView abs1_2;
+	private TextView full_body;
+	private ImageView person_1;
+	private ImageView sleepg_2;
+	private ImageView habitg_3;
+	private ImageView foodg_3;
+	private View rectangle_40;
+	private ImageView sportg_3;
+	private TextView sport_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sport);
+
+		
+		_bg__sport = (View) findViewById(R.id._bg__sport);
+		background_1 = (ImageView) findViewById(R.id.background_1);
+		rectangle_29 = (View) findViewById(R.id.rectangle_29);
+		rectangle_30 = (View) findViewById(R.id.rectangle_30);
+		logotextg_1 = (ImageView) findViewById(R.id.logotextg_1);
+		arrow_1 = (ImageView) findViewById(R.id.arrow_1);
+		legs = (TextView) findViewById(R.id.legs);
+		legs_1 = (ImageView) findViewById(R.id.legs_1);
+		leg_1 = (ImageView) findViewById(R.id.leg_1);
+		arms = (TextView) findViewById(R.id.arms);
+		arm_1 = (ImageView) findViewById(R.id.arm_1);
+		abs = (TextView) findViewById(R.id.abs);
+		abs1_2 = (ImageView) findViewById(R.id.abs1_2);
+		full_body = (TextView) findViewById(R.id.full_body);
+		person_1 = (ImageView) findViewById(R.id.person_1);
+		sleepg_2 = (ImageView) findViewById(R.id.sleepg_2);
+		habitg_3 = (ImageView) findViewById(R.id.habitg_3);
+		foodg_3 = (ImageView) findViewById(R.id.foodg_3);
+		rectangle_40 = (View) findViewById(R.id.rectangle_30);
+		sportg_3 = (ImageView) findViewById(R.id.sportg_3);
+		sport_ek1 = (TextView) findViewById(R.id.sport_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+
+    public void Back(View view) {
+    }
+
+	public void Abs(View view) {
+	}
+
+	public void Legs(View view) {
+	}
+
+	public void Sleep(View view) {
+	}
+
+	public void Habit(View view) {
+	}
+
+	public void Food(View view) {
+	}
+}
+	
+	
